75d1a766fec8e743c8a70f66fb028d38139c276b
apshenichniy <34074932+apshenichniy@users.noreply.github.com>
2023-11-14 04:56:07 +0100

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import { Providers } from "@/components/providers";
 import { cn } from "@/lib/utils";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
